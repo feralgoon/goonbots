71bf3873a25e6d2bf9eddec882f88a825d260e03
feralgoon <joseph.teague@gmail.com>
2018-04-27 20:10:33 -0500

    FirstTestRobot is a very simple bot. When the bot scans another bot, it checks distance. If it's less than 100, the bot fires. If not, it moves half the distance to the bot, then scans again.

---
+++ feralgoon/FirstTestRobot.java
@@ -0,0 +1,37 @@
+package feralgoon;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class FirstTestRobot extends Robot
+{
+    private int turnDirection = 1;
+
+    public void run()
+    {
+        while (true)
+        {
+            turnRight(15 * turnDirection);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e)
+    {
+        if (e.getDistance() > 100)
+        {
+            if (e.getBearing() >= 0) {
+                turnDirection = 1;
+            } else {
+                turnDirection = -1;
+            }
+        }
+        else
+        {
+            fire(25);
+        }
+
+        turnRight(e.getBearing());
+        ahead(e.getDistance()/2);
+        scan();
+    }
+}
